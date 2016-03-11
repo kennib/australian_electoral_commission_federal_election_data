debd60d030df7888df42a547139413ad956d242d
Kenni Bawden <kenni.bawden@gmail.com>
2016-03-12 01:49:08 +1100

    Added a query for candidates that recieved a single vote

---
+++ queries/one-vote.sql
@@ -0,0 +1,16 @@
+SELECT votes, event.name, election.name, contest.name, party.name, candidate.name
+FROM first_preferences AS fp
+JOIN event
+	ON event.id = fp.event_id
+JOIN election
+	ON election.event_id = fp.event_id
+	AND election.id = fp.election_id
+JOIN contest
+	ON contest.event_id = fp.event_id
+	AND contest.election_id = fp.election_id
+	AND contest.id = fp.contest_id
+JOIN party
+	ON fp.party_id = party.id
+JOIN candidate
+	ON candidate.id = fp.candidate_id
+WHERE votes = 1;
